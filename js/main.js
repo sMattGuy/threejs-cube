6b8bf5d8ee929aace22bf3bdcd27319b57418542
MattGuy2 <matthewfrankflammia@gmail.com>
2022-03-25 10:34:20 -0400

    added a smooth wobble to the duck

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import {OrbitControls} from './OrbitControls.js'
-//import { TeapotGeometry } from '../models/TeapotGeometry.js';
 import { GLTFLoader } from './GLTFLoader.js';
 
 let lightColor = 0;
@@ -21,7 +20,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener('change',render);
 controls.minDistance = 5;
 controls.maxDistance = 500;
-controls.enablePan = false;
+controls.enablePan = true;
 
 //ambient light
 const ambient = new THREE.AmbientLight(0x111111,0.1);
@@ -31,67 +30,41 @@ scene.add(ambient);
 let spotLight1 = createSpotlight(0xFF0000);
 let spotLight2 = createSpotlight(0x00FF00);
 let spotLight3 = createSpotlight(0x0000FF);
+let spotLight4 = createSpotlight(0xFFFFFF);
 
 spotLight1.position.set(-5,5,0);
 spotLight2.position.set(5,5,0);
 spotLight3.position.set(0,5,5);
+spotLight4.position.set(0,5,-5);
 
 scene.add(spotLight1);
 scene.add(spotLight2);
 scene.add(spotLight3);
+scene.add(spotLight4);
 //end spotlight
 
 //floor
 let floorMaterial = new THREE.MeshPhongMaterial( { color: 0x808080, dithering: true } );
 let floorGeometry = new THREE.PlaneGeometry( 2000, 2000 );
 let floor = new THREE.Mesh( floorGeometry, floorMaterial );
-floor.position.set( 0, - 1, 0 );
+floor.position.set( 0, 0, 0 );
 floor.rotation.x = - Math.PI * 0.5;
 floor.receiveShadow = true;
 scene.add(floor);
 //end floor
 
-
-//cube
-/*
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshPhongMaterial({color:0xFFFFFF,dithering:true});
-const cube = new THREE.Mesh(boxGeometry,boxMaterial);
-cube.castShadow = true;
-cube.receiveShadow = true;
-scene.add(cube);
-*/
-//end cube
-*/
-/*
-//teapot
-let teapot;
-const teapotSize = 0.5;
-let tess = - 1;	// force initialization
-let bBottom;
-let bLid;
-let bBody;
-let bFitLid;
-let bNonBlinn;
-let shading;
-
-createTeapot();
-//end teapot
-*/
 let duck;
+let path;
 const gltfLoader = new GLTFLoader();
-const url = '../models/Duck.gltf';
-gltfLoader.load(url, (gltf) => {
+
+gltfLoader.load('../models/Duck.gltf', (gltf) => {
 	duck = gltf.scene;
+	duck.position.set(0,0.01,0);
 	scene.add(duck);
 });
 
-const loader = new GLTFLoader();
-loader.load('../models/Pathfinder.glb',function(gltf){
-	scene.add(gltf);
-});
-
 camera.position.z = 10
+camera.position.y = 1;
 
 animate();
 
@@ -102,18 +75,27 @@ function render() {
 function animate(){
 	requestAnimationFrame(animate);
 	
-	//do things to cube
-	/*
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	*/
-	/*
-	teapot.rotation.x += 0.01;
-	teapot.rotation.y += 0.01;
-	*/
-	duck.rotation.x += 0.01;
-	duck.rotation.y += 0.01;
+	duck.rotation.y += 0.03;
+	
+	wobbleDuck();
+	
+	const time = Date.now()*0.0005;
+	
+	spotLight1.position.x = Math.sin(time*0.7)*5;
+	spotLight1.position.z = Math.cos(time*0.7)*5;
+	spotLight2.position.x = -Math.sin(time*0.7)*5;
+	spotLight2.position.z = Math.cos(time*0.7)*5;
+	spotLight3.position.x = Math.sin(time*0.7)*5;
+	spotLight3.position.z = -Math.cos(time*0.7)*5;
+	spotLight4.position.x = -Math.sin(time*0.7)*5;
+	spotLight4.position.z = -Math.cos(time*0.7)*5;
+	
 	renderer.render(scene, camera);
+}
+function wobbleDuck(){
+	const freq = Math.PI;
+	const time = Date.now() * 0.0005;
+	duck.position.y = 0.5 + 0.5 * Math.sin(time*freq);
 }
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
@@ -131,16 +113,3 @@ function createSpotlight(color){
 	spotLight.castShadow = true;
 	return spotLight;
 }
-/*
-function createTeapot(){
-	if(teapot !== undefined){
-		teapot.geometry.dispose();
-		scene.remove(teapot);
-	}
-	const geometry = new TeapotGeometry( teapotSize, tess);
-	let teaMesh = new THREE.MeshPhongMaterial( { color: 0xFFFFFF, dithering: true } );
-	teapot = new THREE.Mesh( geometry, teaMesh);
-
-	scene.add( teapot );
-}
-*/
